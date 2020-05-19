f6c51b1cdac335f95b63124fcadff5e22f45ee5c
Wei Yao <WeiY1@VERIFONE.com>
2020-05-19 00:32:17 -0500

    CWJPMM-1: Complete ui layer, create layout file, WeartherViewModel

--- app/src/main/java/com/wyao/coolweatherjetpack/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/wyao/coolweatherjetpack/ui/weather/WeatherViewModel.kt
@@ -1,11 +1,22 @@
 package com.wyao.coolweatherjetpack.ui.weather
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.wyao.coolweatherjetpack.data.WeatherRepository
+import com.wyao.coolweatherjetpack.data.model.weather.Weather
 
 class WeatherViewModel(private val repository: WeatherRepository) : ViewModel() {
+
+    var weather = MutableLiveData<Weather>()
+
+    var bingPicUrl = MutableLiveData<String>()
+
+    var weatherInitialized = MutableLiveData<Boolean>()
 
     fun getWeather() {
 
     }
+
+    fun refreshWeather() {}
+
 }
