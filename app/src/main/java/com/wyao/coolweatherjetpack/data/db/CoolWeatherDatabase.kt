b0489f5cd59f14b5134673a8a56c92c629ae8307
Wei Yao <mityaowei@hotmail.com>
2020-11-09 00:33:09 -0600

    CWJPMM-2: Complete viewmodel
    1. Complete ChooseAreaViewModel
    2. Complete ChooseAreaFragment

--- app/src/main/java/com/wyao/coolweatherjetpack/data/db/CoolWeatherDatabase.kt
+++ app/src/main/java/com/wyao/coolweatherjetpack/data/db/CoolWeatherDatabase.kt
@@ -4,10 +4,19 @@ object CoolWeatherDatabase {
 
     private var weatherDao: WeatherDao? = null
 
+    private var placeDao: PlaceDao? = null
+
     fun getWeatherDao(): WeatherDao {
         if (weatherDao == null) {
             weatherDao = WeatherDao()
         }
         return weatherDao!!
+    }
+
+    fun getPlaceDao(): PlaceDao {
+        if (placeDao == null) {
+            placeDao = PlaceDao()
+        }
+        return placeDao!!
     }
 }
